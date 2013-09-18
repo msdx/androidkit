18b9431222ac4de95196638a9bc91cc567dc09ee
unknown <msdx@msdx-PC.(none)>
2013-09-19 01:00:27 +0800

    merge Log4AK.java, LogAuto.java from develop branch

--- androidkit/src/com/lurencun/cfuture09/androidkit/utils/lang/Log4AK.java
+++ androidkit/src/com/lurencun/cfuture09/androidkit/utils/lang/Log4AK.java
@@ -20,8 +20,6 @@
  */
 package com.lurencun.cfuture09.androidkit.utils.lang;
 
-import android.util.Log;
-
 import com.lurencun.cfuture09.androidkit.Version;
 
 /**
@@ -33,77 +31,18 @@ import com.lurencun.cfuture09.androidkit.Version;
  *         "http://rescdn.qqmail.com/zh_CN/htmledition/images/function/qm_open/ico_mailme_01.png"
  *         /></a>
  */
-public class Log4AK {
-	public static final int ASSERT = Log.ASSERT;
-	public static final int DEBUG = Log.DEBUG;
-	public static final int ERROR = Log.ERROR;
-	public static final int INFO = Log.INFO;
-	public static final int VERBOSE = Log.VERBOSE;
-	public static final int WARN = Log.WARN;
-	protected final String tag;
+public final class Log4AK extends LogAuto {
 
 	private Log4AK(Class<?> clazz) {
-		tag = String.format("%s: %s", Version.ANDROIDKIT_NAME, clazz.getSimpleName());
+		super(clazz);
+	}
+
+	@Override
+	protected String initTag(Class<?> clazz) {
+		return String.format("%s: %s", Version.ANDROIDKIT_NAME, clazz.getSimpleName());
 	}
 
 	public static final Log4AK getLog(Class<?> clazz) {
 		return new Log4AK(clazz);
 	}
-
-	public void d(String msg) {
-		Log.d(tag, msg);
-	}
-
-	public void d(String msg, Throwable tr) {
-		Log.d(tag, msg, tr);
-	}
-
-	public void e(String msg) {
-		Log.e(tag, msg);
-	}
-
-	public void e(String msg, Throwable tr) {
-		Log.e(tag, msg, tr);
-	}
-
-	public void i(String msg) {
-		Log.i(tag, msg);
-	}
-
-	public void i(String msg, Throwable tr) {
-		Log.i(tag, msg, tr);
-	}
-
-	public void v(String msg) {
-		Log.v(tag, msg);
-	}
-
-	public void v(String msg, Throwable tr) {
-		Log.v(tag, msg, tr);
-	}
-
-	public void w(String msg) {
-		Log.w(tag, msg);
-	}
-
-	public void w(Throwable tr) {
-		Log.w(tag, tr);
-	}
-
-	public void w(String msg, Throwable tr) {
-		Log.w(tag, msg, tr);
-	}
-
-	public void getStackTraceString(Throwable tr) {
-		Log.getStackTraceString(tr);
-	}
-
-	public void isLoggable(int level) {
-		Log.isLoggable(tag, level);
-	}
-
-	public void println(int priority, String msg) {
-		Log.println(priority, tag, msg);
-	}
-
 }
