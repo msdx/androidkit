6339cd39dcc3af7b3344fe94e9551595927dcf49
msdx <645079761@qq.com>
2014-06-18 22:14:24 +0800

    Fix a null pointer exception.

--- androidkit/src/com/lurencun/cfuture09/androidkit/http/async/BinaryHttpResponseHandler.java
+++ androidkit/src/com/lurencun/cfuture09/androidkit/http/async/BinaryHttpResponseHandler.java
@@ -143,7 +143,7 @@ public class BinaryHttpResponseHandler extends AsyncHttpResponseHandler {
 			break;
 		case FAILURE_MESSAGE:
 			response = (Object[]) msg.obj;
-			handleFailureMessage((Throwable) response[0], response[1].toString());
+			handleFailureMessage((Throwable) response[0], response[1]== null ? null :response[1].toString());
 			break;
 		default:
 			super.handleMessage(msg);
